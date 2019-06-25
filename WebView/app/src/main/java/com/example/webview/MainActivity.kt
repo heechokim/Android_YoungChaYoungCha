926148f10a11488b48cc770740c9dee1bb2c91c2
chooeeh <chchgml10@gmail.com>
2019-06-25 15:31:54 +0900

    06/25 웹뷰 연습

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.webview
+
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /** activity_main.xml 뷰 변수 선언 및 초기화 */
+        val myWebView = findViewById<View>(R.id.main_wv) as WebView
+
+        /** 웹뷰 세팅 */
+        val settings = myWebView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+
+        /** 웹뷰 띄우기 */
+        myWebView.loadUrl("https://choheeis.github.io/")
+    }
+}
