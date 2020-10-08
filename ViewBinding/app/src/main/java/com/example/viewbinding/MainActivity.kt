0638101f4fb1636367dbba978d43cc06df385dd5
김초희 <qwerpoiu1316@naver.com>
2020-10-08 18:13:32 +0900

    2020/10/08 ViewBinding

---
+++ ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.ivPicture.setOnClickListener {
+            Toast.makeText(this, "토스트!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
