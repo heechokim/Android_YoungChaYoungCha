235d629f148109b4a8e8d57804127ffc3baed42c
김초희 <qwerpoiu1316@naver.com>
2020-10-28 00:38:05 +0900

    2020/10/28 DataBinding

---
+++ DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+//        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        binding.user = User("Chohee", "Kim")
